f9a3841e9df5fa4e4ef325c77cf1f4dae7586711
Roman Satanovskyi <Roman.Satanovskyi@mindgeek.com>
2019-12-05 13:41:04 +0200

    refactored without REQUIRE

--- metanit2/src/main.ts
+++ metanit2/src/main.ts
@@ -1,9 +1,9 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
-//import { Student } from 'typescript-boilerplate';
-const yourModuleName = require('typescript-boilerplate');
-//let user = new Student("Jane", "M.", "User");
+import { Student } from 'typescript-boilerplate';
+//const Student = require('typescript-boilerplate');
+let user = new Student("Jane", "M.", "User");
 
 const platform = platformBrowserDynamic();
 platform.bootstrapModule(AppModule);

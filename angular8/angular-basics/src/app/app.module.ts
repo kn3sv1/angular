9f3860647593bf31275207d3e2e9dd7c08521451
RSatanovskyi <satcms@hotmail.com>
2020-01-12 18:59:02 +0200

    init

--- angular8/angular-basics/src/app/app.module.ts
+++ angular8/angular-basics/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {PostComponent} from "../post/post.component";
+import {PostComponent} from "./post/post.component";
+import { Post2Component } from './post2/post2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostComponent
+    PostComponent,
+    Post2Component
   ],
   imports: [
     BrowserModule

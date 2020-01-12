8703c025b6694b4ab769e3ca842e06ce05415652
RSatanovskyi <satcms@hotmail.com>
2020-01-12 19:48:37 +0200

    bind event

--- angular8/angular-basics/src/app/app.component.ts
+++ angular8/angular-basics/src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-basics';
+  number = 42;
+  arr = [1,2,3];
+  inputValue = ''
+
+  constructor() {
+  }
+
+  onInput(event?: any) {
+    //console.log('Event', event);
+    this.inputValue = event.target.value;
+  }
 }

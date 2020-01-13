da878ab8a63deb0c4de11fa06980cc73effad369
Roman Satanovskyi <Roman.Satanovskyi@mindgeek.com>
2020-01-13 12:42:20 +0200

    added ngFOR

--- angular8/angular-basics/src/app/app.component.ts
+++ angular8/angular-basics/src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+    title: string,
+    text: string,
+    id?: number
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,14 +14,20 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angular-basics';
   number = 42;
-  arr = [1,2,3];
-  inputValue = ''
+  arr = [1, 1, 2, 3, 4, 5];
+  inputValue = '';
+  backgroundToggle = false;
+
+  posts: Post[] = [
+      {title: 'Title1', text: 'Text1', id: 1},
+      {title: 'Title2', text: 'Text2', id: 2},
+  ];
 
   constructor() {
   }
 
   onInput(event?: any) {
-    //console.log('Event', event);
+    // console.log('Event', event);
     this.inputValue = event.target.value;
   }
 }
